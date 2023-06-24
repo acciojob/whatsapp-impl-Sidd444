d905c804f30683ab2391aeb3846b568fa52e5792
Sidd444 <siddharthabharaliassam@gmail.com>
2023-06-24 22:21:08 +0530

    Assignment done

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,4 +7,33 @@ public class Message {
     private String content;
     private Date timestamp;
 
+    public Message(int id,String content) {
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
